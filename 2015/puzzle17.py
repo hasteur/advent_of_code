719422fe983ebf548c11f7e75225dd2cb601ee60
Hasteur <hasteur@github>
2019-06-30 15:11:12 -0500

    Add code for Puzzle 17

--- 2015/puzzle17.py
+++ 2015/puzzle17.py
@@ -1,15 +1,12 @@
-
+import itertools
 STORE_AMT=150
 
-container_list = []
+containers = []
 fh = open('puzzle17.txt', 'r')
 for line in fh:
-    container_list.append(int(line))
+    containers.append(int(line))
+found_amt = 0
+combinations = [ c for i in xrange(1,len(containers)+1) for c in itertools.combinations(containers,i) if sum(c) == 150]
+print len(combinations)
+print len([c for c in combinations if len(c) == len(min(combinations, key=lambda x:len(x)))])
 
-container_list.sort()
-list_max = container_list[::-1]
-
-base_index = 0
-
-for max_cont_index in range(0,len(list_max)):
-    print list_max[max_cont_index]
